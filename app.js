11ca2a57d043a6b016df306d0018b95a7fede9dd
leogirardeau <leogir.pro@gmail.com>
2020-08-17 09:59:28 +0200

    temperature degree description location

--- app.js
+++ app.js
@@ -2,6 +2,9 @@
 window.addEventListener('load', () => {
     let long;
     let lat;
+    let temperatureDescription = document.querySelector('.temperature-description');
+    let temperatureDegree = document.querySelector('.temperature-degree');
+    let locationTimezone = document.querySelector('.location-timezone');
 
     if (navigator.geolocation) {
         navigator.geolocation.getCurrentPosition(position => {
@@ -16,8 +19,11 @@ window.addEventListener('load', () => {
                     return response.json();
                 })
                 .then(data => {
-                    console.log(data);
                     const { temperature, summary } = data.currently;
+                    //Set DOM Elements from the API
+                    temperatureDegree.textContent = temperature;
+                    temperatureDescription.textContent = summary;
+                    locationTimezone.textContent = data.timezone;
                 });
         });
     } else {
