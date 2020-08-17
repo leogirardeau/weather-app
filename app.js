80d54f25efe7f31e61b43e6c80b5ad2369b67fc9
leogirardeau <leogir.pro@gmail.com>
2020-08-17 09:48:49 +0200

    getting data from api

--- app.js
+++ app.js
@@ -7,6 +7,18 @@ window.addEventListener('load', () => {
         navigator.geolocation.getCurrentPosition(position => {
             long = position.coords.longitude;
             lat = position.coords.latitude;
+
+            const proxy = 'https://cors-anywhere.herokuapp.com/';
+            const api = `${proxy}https://api.darksky.net/forecast/fd9d9c6418c23d94745b836767721ad1/${lat},${long}`;
+
+            fetch(api)
+                .then(response => {
+                    return response.json();
+                })
+                .then(data => {
+                    console.log(data);
+                    const { temperature, summary } = data.currently;
+                });
         });
     } else {
         h1.textContent = "Hey please enable your geolocation or maybe your browser doesn't allow it"
